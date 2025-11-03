9c436bbd5dca6d723e987b932812373206f0cdb6
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2025-11-02 21:41:22 -0700

    HW4-update

--- courses/Fall25/code/HW4/free.c
+++ courses/Fall25/code/HW4/free.c
@@ -1,23 +1,35 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
 
 int
 main(int argc, char *argv[])
 {
     uint64 divisor = 1;
+    const char *unit = "bytes";
 
-    if (argc == 2) {
+    // Optional argument: -k or -m for KB / MB display
+    if (argc == 2 && argv[1][0] == '-') {
         switch (argv[1][1]) {
-	case 'k':	
-	    divisor = 1024;
-	    break;
-	case 'm':
-	    divisor = 1024*1024;
-	    break;
-	}
+        case 'k':
+            divisor = 1024;
+            unit = "KB";
+            break;
+        case 'm':
+            divisor = 1024 * 1024;
+            unit = "MB";
+            break;
+        default:
+            fprintf(2, "Usage: free [-k | -m]\n");
+            exit(1);
+        }
+    } else if (argc > 2) {
+        fprintf(2, "Usage: free [-k | -m]\n");
+        exit(1);
     }
 
-    printf("%l\n", freepmem()/divisor);
+    uint64 free_bytes = freepmem();
+    printf("Free memory: %l %s\n", free_bytes / divisor, unit);
 
     exit(0);
 }
